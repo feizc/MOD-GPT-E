8e91bb872cc00101dd99f8c02769628106ba810d
zcfei <3300949577@qq.com>
2021-05-07 19:05:47 +0800

    up datet

--- data/data_process.py
+++ data/data_process.py
@@ -1,5 +1,6 @@
 import torch 
 import os 
+import json 
 import clip 
 from PIL import Image 
 
@@ -8,12 +9,40 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model, preprocess = clip.load('ViT-B/32', device=device)
 
 # clip feature generation 
-def clip_feature_generate(meme_path): 
+def clip_feature_generate(meme_path, img2id_path): 
+    # read meme list
     meme_name_list = os.listdir(meme_path) 
-    print(len(meme_name_list)) 
+
+    # project meme name to meme id 
+    with open(img2id_path, 'r', encoding='utf-8') as f: 
+        img2id_dict = json.load(f) 
+    # print(img2id_dict.keys())
     
+    id2feature = {} 
+
+    for meme_name in meme_name_list: 
+        if '.DS_Store' in meme_name:
+            continue
+        meme_abs_path = os.path.join(meme_path, meme_name) 
+        image = preprocess(Image.open(meme_abs_path)).unsqueeze(0).to(device) 
+        image_features = model.encode_image(image).tolist()[0] 
+        meme_id = img2id_dict[meme_name] 
+        id2feature[meme_id] = image_features 
+        
+    # save the image feature 
+    id2feature_path = 'data/meme/id2feature.json'
+    with open(id2feature_path, 'w', encoding='utf-8') as f: 
+        #json.dump(id2feature, f, indent=4)
+        json.dump(id2feature, f)
+    
+
+
 
 
 if __name__ == '__main__': 
     meme_path = 'data/meme/image' 
-    clip_feature_generate(meme_path)
+    img2id_path = 'data/meme/img2id.json'
+    # clip_feature_generate(meme_path, img2id_path) 
+    with open('data/meme/id2feature.json', 'r', encoding='utf-8') as f: 
+        id2feature_dict = json.load(f) 
+    print(len(id2feature_dict.keys()))
